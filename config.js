e8b0e3ec7956f05229677be208428d9360668cfd
taideptr4i <125946239+taideptr4i@users.noreply.github.com>
2023-02-21 12:02:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi dợ Thanh Trâm <3",
+    introTitle: 'Bí mật dành cho Dợ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,13 +8,12 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnYes: 'Thích lắm luôn =))',
+    btnNo: 'Không có thích anh :(',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Dợ lại yêu Anh <3',
+    btnReply: 'Gửi cho Anh Taideptr4i chồng Thanh Trâm <3',
+    reply: 'Thích thì yêu',
+    mess: 'Anh biết mà 🥰. Yêu Dợ nhiều nhiều lắm luônnnn 😘😘',
+    messDesc: 'Thích cái này thì hun vào màn hình 3 cái liền luôn đi bé =)).',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    messLink: 'https://fb.com/messages/t/drtaideptr4i' 
